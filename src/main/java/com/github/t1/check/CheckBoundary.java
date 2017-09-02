a2c7a7f7998ac666543550797011bb7c8a411d14
Rüdiger zu Dohna <snackbox@sinntr.eu>
2017-09-02 08:11:41 +0200

    return 500 when a check returns FAILURE

--- src/main/java/com/github/t1/check/CheckBoundary.java
+++ src/main/java/com/github/t1/check/CheckBoundary.java
@@ -3,25 +3,35 @@ package com.github.t1.check;
 import javax.enterprise.inject.Instance;
 import javax.inject.Inject;
 import javax.ws.rs.*;
-import java.util.*;
+import javax.ws.rs.core.Response;
+import java.util.List;
+import java.util.stream.StreamSupport;
+
+import static com.github.t1.check.Status.*;
+import static java.util.stream.Collectors.*;
+import static javax.ws.rs.core.Response.Status.*;
+import static javax.ws.rs.core.Response.Status.OK;
 
 @Path("/-system/check")
 public class CheckBoundary {
     @Inject Instance<Check> checks;
 
-    @GET public CheckResponse get() {
-        return new CheckResponse(collect());
+    @GET public Response get() {
+        CheckResponse checkResponse = new CheckResponse(collect());
+        return Response.status(map(checkResponse.getSummary().getStatus())).entity(checkResponse).build();
     }
 
+    private static Response.Status map(Status status) { return (status == FAILURE) ? INTERNAL_SERVER_ERROR : OK; }
+
     private List<CheckResult> collect() {
-        List<CheckResult> checkResults = new ArrayList<>();
-        checks.forEach(check -> {
-            try {
-                checkResults.add(check.get());
-            } finally {
-                checks.destroy(check);
-            }
-        });
-        return checkResults;
+        return StreamSupport.stream(checks.spliterator(), false)
+                            .map(check -> {
+                                try {
+                                    return check.get();
+                                } finally {
+                                    checks.destroy(check);
+                                }
+                            })
+                            .collect(toList());
     }
 }
--- src/test/java/com/github/t1/check/CheckBoundaryTest.java
+++ src/test/java/com/github/t1/check/CheckBoundaryTest.java
@@ -3,7 +3,11 @@ package com.github.t1.check;
 import com.github.t1.testtools.MockInstance;
 import org.junit.*;
 
+import javax.ws.rs.core.Response;
+
 import static com.github.t1.check.Status.*;
+import static com.github.t1.check.Status.OK;
+import static javax.ws.rs.core.Response.Status.*;
 import static org.assertj.core.api.Assertions.*;
 
 public class CheckBoundaryTest {
@@ -14,63 +18,114 @@ public class CheckBoundaryTest {
     private void givenChecks(Check... items) { boundary.checks = new MockInstance<>(items); }
 
 
-    private static class FooCheck implements Check {
+    private static class OkCheck implements Check {
         @Override public CheckResult get() {
-            return CheckResult.of(FooCheck.class).status(OK).comment("foo").build();
+            return status(OK).comment("foo").build();
         }
     }
 
-    private static class BarCheck implements Check {
-        @Override public CheckResult get() {
-            return CheckResult.of(BarCheck.class).status(WARNING).comment("bar").build();
-        }
+    private static class WarningCheck implements Check {
+        @Override public CheckResult get() { return status(WARNING).comment("bar").build(); }
     }
+
+    private static class UnknownCheck implements Check {
+        @Override public CheckResult get() { return status(UNKNOWN).comment("baz").build(); }
+    }
+
+    private static class FailureCheck implements Check {
+        @Override public CheckResult get() { return status(FAILURE).comment("boo").build(); }
+    }
+
+
+    private CheckResponse checkResponse(Response response) { return (CheckResponse) response.getEntity(); }
+
 
     @Test
     public void shouldReturnEmptyChecks() throws Exception {
-        CheckResponse response = boundary.get();
+        Response response = boundary.get();
 
-        assertThat(response.getSummary().getStatus()).isEqualTo(OK);
-        assertThat(response.getSummary().getCounters()).isEqualTo(Status.mapOf(0, 0, 0, 0));
-        assertThat(response.getChecks()).isEmpty();
+        CheckResponse check = checkResponse(response);
+        assertThat(check.getSummary().getStatus()).isEqualTo(OK);
+        assertThat(check.getSummary().getCounters()).isEqualTo(Status.mapOf(0, 0, 0, 0));
+        assertThat(check.getChecks()).isEmpty();
     }
 
     @Test
     public void shouldReturnOneCheck() throws Exception {
-        givenChecks(new FooCheck());
+        givenChecks(new OkCheck());
 
-        CheckResponse response = boundary.get();
+        Response response = boundary.get();
 
-        assertThat(response.getSummary().getStatus()).isEqualTo(OK);
-        assertThat(response.getSummary().getCounters()).isEqualTo(Status.mapOf(1, 0, 0, 0));
-        assertThat(response.getChecks()).containsExactly(
-                CheckResult.of(FooCheck.class).status(OK).comment("foo").build());
+        CheckResponse check = checkResponse(response);
+        assertThat(check.getSummary().getStatus()).isEqualTo(OK);
+        assertThat(check.getSummary().getCounters()).isEqualTo(Status.mapOf(1, 0, 0, 0));
+        assertThat(check.getChecks()).containsExactly(
+                CheckResult.of(OkCheck.class).status(OK).comment("foo").build());
     }
 
     @Test
     public void shouldReturnTwoChecks() throws Exception {
-        givenChecks(new FooCheck(), new BarCheck());
+        givenChecks(new OkCheck(), new WarningCheck());
 
-        CheckResponse response = boundary.get();
+        Response response = boundary.get();
 
-        assertThat(response.getSummary().getStatus()).isEqualTo(WARNING);
-        assertThat(response.getSummary().getCounters()).isEqualTo(Status.mapOf(1, 0, 1, 0));
-        assertThat(response.getChecks()).containsExactly(
-                CheckResult.of(FooCheck.class).status(OK).comment("foo").build(),
-                CheckResult.of(BarCheck.class).status(WARNING).comment("bar").build());
+        CheckResponse check = checkResponse(response);
+        assertThat(check.getSummary().getStatus()).isEqualTo(WARNING);
+        assertThat(check.getSummary().getCounters()).isEqualTo(Status.mapOf(1, 0, 1, 0));
+        assertThat(check.getChecks()).containsExactly(
+                CheckResult.of(OkCheck.class).status(OK).comment("foo").build(),
+                CheckResult.of(WarningCheck.class).status(WARNING).comment("bar").build());
     }
 
     @Test
     public void shouldReturnThreeChecks() throws Exception {
-        givenChecks(new FooCheck(), new BarCheck(), new BarCheck());
+        givenChecks(new OkCheck(), new WarningCheck(), new WarningCheck());
 
-        CheckResponse response = boundary.get();
+        Response response = boundary.get();
 
-        assertThat(response.getSummary().getStatus()).isEqualTo(WARNING);
-        assertThat(response.getSummary().getCounters()).isEqualTo(Status.mapOf(1, 0, 2, 0));
-        assertThat(response.getChecks()).containsExactly(
-                CheckResult.of(FooCheck.class).status(OK).comment("foo").build(),
-                CheckResult.of(BarCheck.class).status(WARNING).comment("bar").build(),
-                CheckResult.of(BarCheck.class).status(WARNING).comment("bar").build());
+        CheckResponse check = checkResponse(response);
+        assertThat(check.getSummary().getStatus()).isEqualTo(WARNING);
+        assertThat(check.getSummary().getCounters()).isEqualTo(Status.mapOf(1, 0, 2, 0));
+        assertThat(check.getChecks()).containsExactly(
+                CheckResult.of(OkCheck.class).status(OK).comment("foo").build(),
+                CheckResult.of(WarningCheck.class).status(WARNING).comment("bar").build(),
+                CheckResult.of(WarningCheck.class).status(WARNING).comment("bar").build());
+    }
+
+
+    @Test
+    public void shouldMapOkCheckTo200() throws Exception {
+        givenChecks(new OkCheck());
+
+        Response response = boundary.get();
+
+        assertThat(response.getStatusInfo()).isEqualTo(Response.Status.OK);
+    }
+
+    @Test
+    public void shouldMapWarningCheckTo200() throws Exception {
+        givenChecks(new WarningCheck());
+
+        Response response = boundary.get();
+
+        assertThat(response.getStatusInfo()).isEqualTo(Response.Status.OK);
+    }
+
+    @Test
+    public void shouldMapUnknownCheckTo200() throws Exception {
+        givenChecks(new UnknownCheck());
+
+        Response response = boundary.get();
+
+        assertThat(response.getStatusInfo()).isEqualTo(Response.Status.OK);
+    }
+
+    @Test
+    public void shouldMapFailiureCheckTo500() throws Exception {
+        givenChecks(new FailureCheck());
+
+        Response response = boundary.get();
+
+        assertThat(response.getStatusInfo()).isEqualTo(INTERNAL_SERVER_ERROR);
     }
 }
